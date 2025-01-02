01c7765e4524521acaa9091a82867e9a5529aec5
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-01-02 19:54:25 +0530

    EbayDemo.java modified.

--- src/test/java/EbayDemo.java
+++ src/test/java/EbayDemo.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class EbayDemo {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class EbayDemo {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+//        Thread.sleep(2000);
+        driver.manage().window().maximize();
+//        Thread.sleep(2000);
+        driver.get("https://www.ebay.com/");
+//        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id='gh-ac']")).click();
+        driver.findElement(By.xpath("//input[@id='gh-ac']")).clear();
+        driver.findElement(By.xpath("//input[@id='gh-ac']")).sendKeys("Iphone");
+
+        Select selectCategory = new Select(driver.findElement(By.id("gh-cat")));
+        selectCategory.selectByVisibleText("Cell Phones & Accessories");
+
+        driver.findElement(By.cssSelector("#gh-btn")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//span[text()='Apple iPhone 11']")).click();
+        Thread.sleep(3000);
+
+
+    }
 }
