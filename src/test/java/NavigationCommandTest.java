2bb66508ca4b55ba6b686b3f6f9f7c538a1d6c36
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-01-09 19:44:09 +0530

    Create NavigationCommandTest.java

---
+++ src/test/java/NavigationCommandTest.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NavigationCommandTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+
+//        Thread.sleep(2000);
+        driver.manage().window().maximize();
+        String url = "https://www.ebay.com/";
+
+//       Opens the specified URL in the browser
+        driver.get(url);
+
+        System.out.println("1 : "+driver.getTitle());
+
+        driver.findElement(By.xpath("//li/a[text()='Sports']")).click();
+        Thread.sleep(2000);
+        System.out.println("2 : "+driver.getTitle());
+
+        driver.navigate().back();
+        Thread.sleep(2000);
+        System.out.println("3 : "+driver.getTitle());
+
+        driver.navigate().forward();
+        Thread.sleep(2000);
+        System.out.println("4 : "+driver.getTitle());
+
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+        
+        driver.quit();
+    }
+}
