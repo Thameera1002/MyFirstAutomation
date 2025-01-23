76a4416c2f171418e86f8426db3b60bf28b013c9
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-01-23 19:38:44 +0530

    TestNGAssertTest> softassertions added

--- src/test/java/testng/TestNGAssertTest.java
+++ src/test/java/testng/TestNGAssertTest.java
@@ -2,6 +2,7 @@ package testng;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class TestNGAssertTest {
 
@@ -14,5 +15,17 @@ public class TestNGAssertTest {
         Assert.assertEquals(name,"Amal");
         System.out.println("C");
         System.out.println("D");
+    }
+
+    @Test
+    public void softAssertion(){
+        SoftAssert softAssert = new SoftAssert();
+        String name ="Kamal"; //xpath.getText();
+        System.out.println("A");
+        System.out.println("B");
+        softAssert.assertEquals(name,"Amal");
+        System.out.println("C");
+        System.out.println("D");
+        softAssert.assertAll();
     }
 }
